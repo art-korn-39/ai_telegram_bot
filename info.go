7b4cd44ea33544d8a87d59953acf0d9500ff6fa4
art39 <art39hb@yandex.ru>
2024-02-14 07:38:50 +0300

    info with sum operations for today

--- info.go
+++ info.go
@@ -7,14 +7,14 @@ import (
 
 func GetInfo(isRO bool) string {
 
-	Now := time.Now().UTC().Add(3 * time.Hour)
+	Now := time.Now().UTC().Add(3 * time.Hour) // 03.02.24 14:50
 
 	// Если это регламентная операция, то берем предыдущий день
 	if isRO {
-		Now = Now.AddDate(0, 0, -1)
+		Now = Now.AddDate(0, 0, -1) // 02.02.24 14:50
 	}
 
-	StartDay := time.Date(Now.Year(), Now.Month(), Now.Day(), 0, 0, 0, 0, Now.Location())
+	StartDay := time.Date(Now.Year(), Now.Month(), Now.Day(), 0, 0, 0, 0, Now.Location()) // 03.02.24 00:00
 	December29 := time.Date(2023, 12, 29, 0, 0, 0, 0, time.Local)
 	December25 := time.Date(2023, 12, 25, 0, 0, 0, 0, time.Local)
 
@@ -40,15 +40,16 @@ func GetInfo(isRO bool) string {
 
 	return fmt.Sprintf(
 		`All time [%d]
-Users: %d		
+Users: %d	
 Gemini: %d | ChatGPT: %d | Kandinsky: %d | SDXL: %d
 
-Today
+Today [%d]
 Users: %d (new: %d)
 Gemini: %d | ChatGPT: %d | Kandinsky: %d | SDXL: %d`,
 		result_dec25["gemini"]+result_dec25["chatgpt"]+result_dec25["kandinsky"]+result_dec25["sdxl"],
 		result_dec29["users"],
 		result_dec25["gemini"], result_dec25["chatgpt"], result_dec25["kandinsky"], result_dec25["sdxl"],
+		result_Today["gemini"]+result_Today["chatgpt"]+result_Today["kandinsky"]+result_Today["sdxl"],
 		result_Today["users"], newUsersToday,
 		result_Today["gemini"], result_Today["chatgpt"], result_Today["kandinsky"], result_Today["sdxl"],
 	)
