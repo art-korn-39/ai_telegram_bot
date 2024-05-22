d40cc197a2c689fb1924a67516842abf4bb8dd79
art39 <art39hb@yandex.ru>
2024-05-22 16:34:18 +0400

    2.6.0 bug fix sdxl

--- SDXL_upscale.go
+++ SDXL_upscale.go
@@ -62,8 +62,8 @@ func sdxl_Image2ImageUpscale(user *UserInfo, filepath string) (result string, er
 		}
 		var body res_body
 
-		if res.StatusCode == 522 || res.StatusCode == 520 {
-			err = errors.New("522 status code =/")
+		if res.StatusCode/100 == 5 { //522, 520, 503
+			err = errors.New("5XX status code =/")
 			msgText = GetText(MsgText_APIdead, user.Language)
 		} else if err = json.NewDecoder(res.Body).Decode(&body); err != nil {
 			err = errors.New("{Decode(&body) 1} " + err.Error())
